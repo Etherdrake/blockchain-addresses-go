57894800501f1bf9c9fab530452d26c76fc3c7aa
Etherdrake <67021215+Etherdrake@users.noreply.github.com>
2024-04-08 04:38:28 -0400

    Ethereum token constants

---
+++ ethereum.go
@@ -0,0 +1,10 @@
+package blockchain_addresses
+
+const (
+	DAI_ETH  = "0x6b175474e89094c44da98b954eedeac495271d0f"
+	WETH_ETH = "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2"
+	USDC_ETH = "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48"
+	WBTC_ETH = "0x2260fac5e5542a773aa44fbcfedf7c193bc2c599"
+	USDT_ETH = "0xdac17f958d2ee523a2206206994597c13d831ec7"
+	LINK_ETH = "0x514910771af9ca656af840dff83e8264ecf986ca"
+)
